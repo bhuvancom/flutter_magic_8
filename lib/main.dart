db87f4b20c164f14a2deb6f1f323f4ed5139530f
bhuvaneshvar <bhuvaneshvar6@outlook.com>
2019-12-08 11:00:11 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,63 @@
+import 'dart:math' show Random;
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: MainScreen(),
       ),
     );
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  var img = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        child: Image.asset(
+          'images/ball$img.png',
+        ),
+        onPressed: () {
+          switchImg();
+        },
+      ),
+    );
+  }
+
+  void switchImg() {
+    setState(() {
+      img = Random().nextInt(5) + 1;
+    });
+  }
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text(
+            'Ask Me Anything',
+          ),
+        ),
+        body: Container(
+          color: Colors.cyan,
+          child: Ball(),
+        ),
+      ),
+    );
+  }
+}
